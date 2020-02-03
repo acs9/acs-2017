020eb9492831cabd6826f794a5539e5799fb44ce
Diptanshu <diptanshu@sumitek.net>
2020-02-03 10:59:04 +0530

    2020 update
    seo etc update 2020

---
+++ __submit.php
@@ -0,0 +1,25 @@
+<?php
+if($_POST['submit'] =='submit'){
+$email = isset($_POST['email']) ? $_POST['email'] :'';
+$mobile = isset($_POST['mobile']) ? $_POST['mobile'] :'';
+$Charter = isset($_POST['service']) ? $_POST['service'] :'';
+
+$fullname = isset($_POST['name']) ? $_POST['name'] :'';
+
+$to ="mathew@aircharterservices.com";
+$subject = 'Enquiry from '.ucwords($fullname).','.$mobile.', for '.$Charter.'';
+	$txt= "Name : ".ucwords($fullname)."\r\n Email: ".$email."\r\n Mobile: ".$mobile."\r\nEnquiry: ".$Charter;
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
+
+// More headers
+$headers .= 'From: <contact@aircharterservices.com>' . "\r\n";
+$headers .= 'Bcc: ds99@gmx.us' . "\r\n";
+//$headers .= 'Cc: mathew@aircharterservices.com' . "\r\n";
+
+$isResponse = mail($to,$subject,$txt,$headers);
+header('Location:thankyou.html');
+}else{
+header('Location:booknow.html');
+}
+?>
